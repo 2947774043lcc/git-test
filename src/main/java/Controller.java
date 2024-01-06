70da7f8fec2663671e5acbc8d96118f381d31776
2947774043lcc <155670724+2947774043lcc@users.noreply.github.com>
2024-01-06 16:31:52 +0800

    Update Controller.java

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,6 +1,8 @@
 public class Controller {
    public static void main(String[] args) {
         System.out.println("控制器");
+        System.out.println("控制器");
+        System.out.println("控制器");
     }
 }
 
